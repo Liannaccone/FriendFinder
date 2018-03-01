845671c67cba4341250ef63330ee781878f2671b
liannaccone <lciannaccone@gmail.com>
2018-03-01 08:59:44 -0800

    adding photos to friendsArray, algorithm to match user with a friend, and res sending the name/photo to html

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,23 +3,28 @@
 var friendsArray = [
 	{
 		name: "Handsome Dan",
-		photo: "",
+		photo: "https://cdn.empireonline.com/jpg/80/0/0/1200/675/0/0/0/0/0/0/0/c/articles/59b858d7b43fedb81e7262aa/nicolas-cage.jpg",
 		answers: [1,4,2,5,4,3,2,3,5,2]
 	},
 	{
 		name: "Lonely Lorenzo",
-		photo: "",
+		photo: "https://www.famousbirthdays.com/faces/cage-nicolas-image.jpg",
 		answers: [4,3,5,2,1,2,5,4,5,3]
 	},
 	{
 		name: "Available Arnold",
-		photo: "",
+		photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c0/Nicolas_Cage_Deauville_2013.jpg/220px-Nicolas_Cage_Deauville_2013.jpg",
 		answers: [1,1,1,3,2,5,4,3,2,3]
 	},
 	{
 		name: "Open-relationship Otto",
-		photo: "",
+		photo: "https://nerdist.com/wp-content/uploads/2015/12/Nicolas-Cage-Con-Air.jpg",
 		answers: [5,5,5,4,3,4,3,5,2,3]
+	},
+	{
+		name: "Wetblanket Will",
+		photo: "https://images.huffingtonpost.com/gen/154482/NICOLAS-CAGE-BLONDE.jpg",
+		answers: [1,1,1,1,1,1,1,1,1,1]
 	}
 ];
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,35 +2,42 @@ var friendsArray = require("../data/friends.js");
 
 module.exports = function(app) {
 
-app.post("/survey", function(req, res) {
-	// friends.push(req.body);
-	console.log(req.body)
-	console.log("\nanswers: "+req.body.answers);
-	res.json(true);
+	app.post("/survey", function(req, res) {
+		// friends.push(req.body);
+		var newFriendName = req.body.name;
+		var newFriendPhoto = req.body.photo;
+		var newFriendAnswers = req.body.answers;
+		
+
+	// vaiable to hold friendsArray.name of the current match
+	var curMatch;
+	var curMatchPhoto;
+	// variable set to an arbitrary high number, lowered as user iterates through potential matches if curFriendScore < matchScore, then matchScore = curFriendScore
+	var matchScore = 1000
+
+	for (i = 0; i<friendsArray.length; i++){
+		var curFriendAnswers = friendsArray[i].answers;
+		var curFriendScore = 0
+		for(j = 0; j< curFriendAnswers.length; j++) {
+			var num1 = newFriendAnswers[j];
+			var num2 = curFriendAnswers[j];
+			var diff = Math.abs(num1 - num2);
+			curFriendScore += diff
+		}
+		
+		if(curFriendScore < matchScore) {
+			curMatch = friendsArray[i].name;
+			curMatchPhoto = friendsArray[i].photo;
+			matchScore = curFriendScore
+		}
+	}
+
+	console.log(curMatch);
+	console.log(matchScore)
+	res.json("You matched with " + curMatch +"!"+"\n<img src="+ curMatchPhoto +" height=42width=42>")
 
 
-});
-
+	});
 
 }
 
-// vaiable to hold friendsArray.name of the current match
-// var curMatch;
-// // variable set to an arbitrary high number, lowered as user iterates through potential matches if curFriendScore < matchScore, then matchScore = curFriendScore
-// var matchScore = 1000
-
-// for (i = 0; i<friendsArray.length; i++){
-// 	var curFriendAnswers = friendsArray[i].answers;
-// 	var curFriendScore = 0
-// 	for(j = 0; j< curFriendAnswers.length; j++) {
-// 		var num1 = userAnswers[j];
-// 		var num2 = curFriendAnswers[j];
-// 		var diff = Math.abs(num1 - num2);
-// 		curFriendScore += diff
-// 	}
-	
-// 	if(curFriendScore < matchScore) {
-// 		curMatch = friendsArray[i].name;
-// 		matchScore = curFriendScore
-// 	}
-// }
