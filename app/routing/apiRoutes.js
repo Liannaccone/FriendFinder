bbfa962148af6afb1a475ead30db2590841e6a05
liannaccone <lciannaccone@gmail.com>
2018-03-01 17:05:12 -0800

    adding initial modal html

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,36 +7,29 @@ module.exports = function(app) {
 		var newFriendName = req.body.name;
 		var newFriendPhoto = req.body.photo;
 		var newFriendAnswers = req.body.answers;
-		
-
-	// vaiable to hold friendsArray.name of the current match
-	var curMatch;
-	var curMatchPhoto;
-	// variable set to an arbitrary high number, lowered as user iterates through potential matches if curFriendScore < matchScore, then matchScore = curFriendScore
-	var matchScore = 1000
-
-	for (i = 0; i<friendsArray.length; i++){
-		var curFriendAnswers = friendsArray[i].answers;
-		var curFriendScore = 0
-		for(j = 0; j< curFriendAnswers.length; j++) {
-			var num1 = newFriendAnswers[j];
-			var num2 = curFriendAnswers[j];
-			var diff = Math.abs(num1 - num2);
-			curFriendScore += diff
+		// vaiable to hold friendsArray.name of the current match
+		var curMatch;
+		var curMatchPhoto;
+		// variable set to an arbitrary high number, lowered as user iterates through potential matches if curFriendScore < matchScore, then matchScore = curFriendScore
+		var matchScore = 1000
+		for (i = 0; i<friendsArray.length; i++){
+			var curFriendAnswers = friendsArray[i].answers;
+			var curFriendScore = 0
+			for(j = 0; j< curFriendAnswers.length; j++) {
+				var num1 = newFriendAnswers[j];
+				var num2 = curFriendAnswers[j];
+				var diff = Math.abs(num1 - num2);
+				curFriendScore += diff
+			}
+			if(curFriendScore < matchScore) {
+				curMatch = friendsArray[i].name;
+				curMatchPhoto = friendsArray[i].photo;
+				matchScore = curFriendScore
+			}
 		}
-		
-		if(curFriendScore < matchScore) {
-			curMatch = friendsArray[i].name;
-			curMatchPhoto = friendsArray[i].photo;
-			matchScore = curFriendScore
-		}
-	}
-
-	console.log(curMatch);
-	console.log(matchScore)
-	res.json("You matched with " + curMatch +"!"+"\n<img src="+ curMatchPhoto +" height=42width=42>")
-
-
+		console.log(curMatch);
+		console.log(matchScore)
+		res.json("You matched with " + curMatch +"!"+"\n<img src="+ curMatchPhoto +" height=42width=42>")
 	});
 
 
